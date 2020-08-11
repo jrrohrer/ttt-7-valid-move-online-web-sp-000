3e812e6e28d000bc51263228d4c510dcb4e34f54
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-11 20:40:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,18 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken? == false
-    return true
-  else if position_taken? == true
     return false
+  else if position_taken? == true
+    return true
+  end
+end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] ==""
+    return false
+  else if board[index] == "X" || board[index] == "O"
+    return true
   end
 end
 end

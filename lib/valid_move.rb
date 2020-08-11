f35db2fd547a397db4a65fca07ac0d40d4e02cfa
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-11 20:38:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,12 +8,4 @@ def valid_move?(board, index)
 end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(array, index)
-  if array[index] == " " || array[index] == ""
-    return false
-  else if array[index] == "X" || array[index] == "O"
-    return true
-  end
-end
-end
+
